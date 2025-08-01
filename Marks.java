62dc6c1e64ac9d58615bf2c7e99507a8944c9055
IT24103479 <it24103479@my.sliit.lk>
2025-08-01 12:03:10 +0530

    Add files via upload

---
+++ Marks.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Marks{
+public static void main(String[] args){
+     int[] studentID=new int[3];
+     int[][] update=new int[3][3];
+     int[] average_s=new int[3];
+     int[] average=new int[3];
+     int[] total=new int[3];
+
+ Scanner sc=new Scanner(System.in);
+
+
+
+}
+}
