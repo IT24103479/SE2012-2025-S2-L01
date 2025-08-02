dc3b5974ccb8d85b6aed3a3a7ca2ca6e5d1b6781
IT24103479 <it24103479@my.sliit.lk>
2025-08-02 17:30:07 +0530

    Question 1

--- Welcome.java
+++ Welcome.java
@@ -10,6 +10,6 @@ String input1=sc.nextLine();
 System.out.print("Enter your Last name:");
 String input2=sc.nextLine();
 
- System.out.print("Welcome to the second year "+input1+" "+input2);
+ System.out.printf("Welcome to the second year %s %s ",input1,input2);
 }
 }
